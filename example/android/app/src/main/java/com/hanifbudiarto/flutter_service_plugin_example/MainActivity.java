1f6eb7d21ab53d26b4c5dd745408f01a057ff0ce
Hanif Budiarto <m.hanifbudiarto@gmail.com>
2019-04-08 12:12:10 +0700

    start sam service as foreground service

--- example/android/app/src/main/java/com/hanifbudiarto/flutter_service_plugin_example/MainActivity.java
+++ example/android/app/src/main/java/com/hanifbudiarto/flutter_service_plugin_example/MainActivity.java
@@ -1,9 +1,6 @@
 package com.hanifbudiarto.flutter_service_plugin_example;
 
-import android.content.Intent;
 import android.os.Bundle;
-
-import com.hanifbudiarto.flutter_service_plugin.service.NetworkChangeService;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
